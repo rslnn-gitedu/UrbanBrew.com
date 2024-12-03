de6622913f36c347161e5f32281427ac9b2c4b98
rslnn-gitedu <ra.tapulayan00790@student.tsu.edu.ph>
2024-12-03 13:03:14 +0800

    Update Menu.js

--- Javascript/Menu.js
+++ Javascript/Menu.js
@@ -1,16 +1,10 @@
 window.onload = function() {
-    // Get all the buttons with the 'Tab' class inside links
     var buttons = document.querySelectorAll('.NavigationItems a button');
-
-    // Get the current page URL
     var currentPage = window.location.pathname;
 
-    // Loop through all buttons and add the 'active' class to the one that matches the current page
     buttons.forEach(function(button) {
-        // Get the href attribute of the <a> tag
         var buttonLink = button.parentElement.getAttribute('href'); 
         
-        // Check for exact match between current page and button's href
         if (currentPage === buttonLink) {
             button.classList.add('active');
         } else {
@@ -22,10 +16,9 @@ window.onload = function() {
 
     function toggleMenu() {
         const menu = document.querySelector('.NavigationItems');
-        menu.classList.toggle('show'); // Toggle the "show" class to display or hide
+        menu.classList.toggle('show');
     }
 
-    // Optional: Close the nav when clicking outside of it
     document.addEventListener('click', function (event) {
         const menu = document.querySelector('.NavigationItems');
         const hamburgerMenu = document.querySelector('.hamburger');
